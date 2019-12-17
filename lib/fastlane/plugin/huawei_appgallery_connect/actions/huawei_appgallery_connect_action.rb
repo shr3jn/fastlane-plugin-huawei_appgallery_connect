37546df6e0382e144b421092c35e608c6be1f583
Shreejan Shrestha <shr3jn@gmail.com>
2019-12-17 22:06:36 +0545

    Adds phase wise release, time based released, changelog update, privacy policy url update

--- lib/fastlane/plugin/huawei_appgallery_connect/actions/huawei_appgallery_connect_action.rb
+++ lib/fastlane/plugin/huawei_appgallery_connect/actions/huawei_appgallery_connect_action.rb
@@ -10,10 +10,15 @@ module Fastlane
         if token.nil?
           UI.message("Cannot retrieve token, please check your client ID and client secret")
         else
+
+          if params[:privacy_policy_url] != nil
+            Helper::HuaweiAppgalleryConnectHelper.update_appinfo(params[:client_id], token, params[:app_id], params[:privacy_policy_url])
+          end
+
           upload_app = Helper::HuaweiAppgalleryConnectHelper.upload_app(token, params[:client_id], params[:app_id], params[:apk_path])
 
           if upload_app
-            Helper::HuaweiAppgalleryConnectHelper.submit_app_for_review(token, params[:client_id], params[:app_id])
+            Helper::HuaweiAppgalleryConnectHelper.submit_app_for_review(token, params)
           end
         end
         # Helper::HuaweiAppgalleryConnectHelper.getAppInfo(token, params[:client_id], params[:app_id])
@@ -60,6 +65,56 @@ module Fastlane
                                        env_name: "HUAWEI_APPGALLERY_CONNECT_APK_PATH",
                                        description: "Path to APK file for upload",
                                        optional: false,
+                                       type: String),
+
+          FastlaneCore::ConfigItem.new(key: :changelog_path,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_CHANGELOG_PATH",
+                                     description: "Path to Changelog file (Default empty)",
+                                     optional: true,
+                                     type: String),
+
+          FastlaneCore::ConfigItem.new(key: :privacy_policy_url,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_PRIVACY",
+                                     description: "Privacy Policy URL",
+                                     optional: true,
+                                     type: String),
+
+          FastlaneCore::ConfigItem.new(key: :phase_wise_release,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_PHASE_WISE_RELEASE",
+                                     description: "Phase wise release",
+                                     optional: true,
+                                     conflicting_options: [:release_time],
+                                     type: Boolean),
+
+          FastlaneCore::ConfigItem.new(key: :phase_release_start_time,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_PHASE_WISE_RELEASE_START_TIME",
+                                     description: "Start time of the validity period of the release by phase. The value is UTC time in the following format: yyyy-MM-dd 'T' HH:mm:ssZZ",
+                                     optional: true,
+                                     type: String),
+
+          FastlaneCore::ConfigItem.new(key: :phase_release_end_time,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_PHASE_WISE_RELEASE_END_TIME",
+                                     description: "End time of the validity period of the release by phase. The value is UTC time in the following format: yyyy-MM-dd 'T' HH:mm:ssZZ",
+                                     optional: true,
+                                     type: String),
+
+          FastlaneCore::ConfigItem.new(key: :phase_release_percent,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_PHASE_WISE_RELEASE_PERCENT",
+                                     description: "Percentage of the release by phase. The value must be accurate to two decimal places and does not contain the percent sign (%)",
+                                     optional: true,
+                                     type: String),
+
+          FastlaneCore::ConfigItem.new(key: :phase_release_description,
+                                     env_name: "HUAWEI_APPGALLERY_CONNECT_PHASE_WISE_RELEASE_DESCRIPTION",
+                                     description: "Phase-based release description. (Max 500 characters)",
+                                     optional: true,
+                                     type: String),
+
+          FastlaneCore::ConfigItem.new(key: :release_time,
+                                       env_name: "HUAWEI_APPGALLERY_CONNECT_RELEASE_TIME",
+                                       description: "Release time in UTC format for app release on a specific date. The format is yyyy-MM-dd'T'HH:mm:ssZZ)",
+                                       optional: true,
+                                       conflicting_options: [:phase_wise_release],
                                        type: String)
         ]
       end
--- lib/fastlane/plugin/huawei_appgallery_connect/helper/huawei_appgallery_connect_helper.rb
+++ lib/fastlane/plugin/huawei_appgallery_connect/helper/huawei_appgallery_connect_helper.rb
@@ -6,7 +6,7 @@ module Fastlane
   module Helper
     class HuaweiAppgalleryConnectHelper
       def self.get_token(client_id, client_secret)
-        UI.message("Fetching app access token")
+        UI.important("Fetching app access token")
 
         uri = URI('https://connect-api.cloud.huawei.com/api/oauth2/v1/token')
         http = Net::HTTP.new(uri.host, uri.port)
@@ -33,9 +33,32 @@ module Fastlane
         response = http.request(request)
 
         result_json = JSON.parse(res.body)
-
-        UI.message(response.body)
       end
+
+      def self.update_appinfo(client_id, token, app_id, privacy_policy_url)
+        UI.important("Updating app info")
+
+        uri = URI.parse("https://connect-api.cloud.huawei.com/api/publish/v2/app-info?appId=#{app_id}")
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Put.new(uri.request_uri)
+        request["client_id"] = client_id
+        request["Authorization"] = "Bearer #{token}"
+
+        request.body = {privacyPolicy: privacy_policy_url}.to_json
+
+        response = http.request(request)
+
+        result_json = JSON.parse(response.body)
+
+        if result_json['ret']['code'] == 0
+          UI.success("Successfully updated app info")
+        else
+          UI.user_error!("Failed to update app info")
+        end
+      end
+
 
       def self.upload_app(token, client_id, app_id, apk_path)
         UI.message("Fetching upload URL")
@@ -47,15 +70,16 @@ module Fastlane
         request = Net::HTTP::Get.new(uri.request_uri)
         request["client_id"] = client_id
         request["Authorization"] = "Bearer #{token}"
+
         response = http.request(request)
 
         result_json = JSON.parse(response.body)
 
         if result_json['uploadUrl'].nil?
-          UI.message('Cannot obtain upload url')
+          UI.user_error!('Cannot obtain upload url')
           return false
         else
-          UI.message('Uploading app')
+          UI.important('Uploading app')
           # Upload App
           boundary = "755754302457647"
           uri = URI(result_json['uploadUrl'])
@@ -86,8 +110,8 @@ module Fastlane
           result_json = JSON.parse(result.body)
 
           if result_json['result']['result_code'].to_i == 0
-            UI.message('Upload app to AppGallery Connect successful')
-            UI.message("Saving app information")
+            UI.success('Upload app to AppGallery Connect successful')
+            UI.important("Saving app information")
 
             uri = URI.parse("https://connect-api.cloud.huawei.com/api/publish/v2/app-file-info?appId=#{app_id}")
 
@@ -97,7 +121,7 @@ module Fastlane
             request["client_id"] = client_id
             request["Authorization"] = "Bearer #{token}"
 
-            data = {fileType: 5, lang: 'en-GB', files: [{
+            data = {fileType: 5, lang: "en-GB", files: [{
 
                 fileName: "release.apk",
                 fileDestUrl: result_json['result']['UploadFileRsp']['fileInfoList'][0]['fileDestUlr'],
@@ -111,10 +135,10 @@ module Fastlane
             result_json = JSON.parse(response.body)
 
             if result_json['ret']['code'] == 0
-              UI.message("App information saved.")
+              UI.success("App information saved.")
               return true
             else
-              UI.message("Failed to save app information")
+              UI.user_error!("Failed to save app information")
               return false
             end
           else
@@ -123,24 +147,68 @@ module Fastlane
         end
       end
 
-      def self.submit_app_for_review(token, client_id, app_id)
-        UI.message("Submitting app for review")
+      def self.submit_app_for_review(token, params)
+        UI.important("Submitting app for review")
 
-        uri = URI.parse("https://connect-api.cloud.huawei.com/api/publish/v2/app-submit?appId=#{app_id}")
+        release_type = ''
+        release_time = ''
+
+        if (params[:phase_wise_release] != nil && params[:phase_wise_release]) && (
+              params[:phase_release_start_time] == nil ||
+              params[:phase_release_end_time] == nil ||
+              params[:phase_release_percent] == nil ||
+              params[:phase_release_description] == nil
+        )
+          UI.user_error!("Submit for review failed. Phase wise release requires Start time, End time Release Percent & Descrption")
+          return
+        elsif params[:phase_wise_release] != nil && params[:phase_wise_release]
+          release_type = '&releaseType=3'
+        end
+
+        if params[:release_time] != nil
+          params[:release_time] = URI::encode(params[:release_time], /\W/)
+          release_time = "&releaseTime=#{params[:release_time]}"
+        end
+
+        changelog = ''
+
+        if params[:changelog_path] != nil
+          changelog_data = File.read(params[:changelog_path])
+
+          if changelog_data.length < 3 || changelog_data.length > 300
+            UI.user_error!("Failed to submit app for review. Changelog file length is invalid")
+            return
+          else
+            changelog = "&remark=" + URI::encode(changelog_data)
+          end
+        end
+
+        uri = URI.parse("https://connect-api.cloud.huawei.com/api/publish/v2/app-submit?appId=#{params[:app_id]}" + changelog + release_type + release_time)
 
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         request = Net::HTTP::Post.new(uri.request_uri)
-        request["client_id"] = client_id
+        request["client_id"] = params[:client_id]
         request["Authorization"] = "Bearer #{token}"
+
+        if params[:phase_wise_release] != nil && params[:phase_wise_release]
+          request.body = {
+              phasedReleaseStartTime: params[:phase_release_start_time],
+              phasedReleaseEndTime: params[:phase_release_end_time],
+              phasedReleasePercent: params[:phase_release_percent],
+              phasedReleaseDescription: params[:phase_release_description]
+          }.to_json
+        end
+
+
         response = http.request(request)
 
         result_json = JSON.parse(response.body)
 
         if result_json['ret']['code'] == 0
-            UI.message("Successfully submitted app for review")
+            UI.success("Successfully submitted app for review")
         else
-          UI.message("Failed to submit app for review")
+          UI.user_error!("Failed to submit app for review")
         end
 
       end
